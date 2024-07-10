d8b0f7e3edd1cc2e6c28ef187c07969cd68a53c7
izaleski <iwo.zaleski@payback.net>
2024-07-10 12:30:33 +0200

    Added configuration for creation of service accounts and roles for them

---
+++ main.tf
@@ -0,0 +1,38 @@
+module "service_accounts" {
+  source = "github.com/StrikerASD/IAM"
+
+  project_id = "sb-izal-20240703-111957"
+
+  service_accounts = {
+    "cluster-service-accoount@sb-izal-20240703-111957.iam.gserviceaccount.com" = {
+      display_name = "Cluster SA"
+      roles        = [
+        "roles/viewer",
+        "roles/storage.admin"
+      ]
+    },
+    "jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com" = {
+      display_name = "Jenkins builder SA"
+      roles        = [
+        "roles/cloudkms.admin",
+        "roles/cloudkms.verifier",
+        "roles/container.admin",
+        "roles/resourcemanager.projectIamAdmin",
+        "roles/owner",
+        "roles/iam.serviceAccountAdmin",
+        "roles/iam.serviceAccountTokenCreator",
+        "roles/storage.admin"
+      ]
+    }
+    "service-376765086748@gs-project-accounts.iam.gserviceaccount.com" = {
+      display_name = "Cloud Storage SA"
+      roles        = [
+        "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+      ]
+    }
+  }
+}
+
+output "service_account_emails" {
+  value = module.service_accounts.service_account_emails
+}
