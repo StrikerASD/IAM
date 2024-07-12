01ef1597c55988db5b5ce2bf5b8013229cc5dd6f
izaleski <iwo.zaleski@payback.net>
2024-07-12 08:28:46 +0200

    Commented service_account_emails output

--- main.tf
+++ main.tf
@@ -3,36 +3,26 @@ module "IAM" {
 
   project_id = "sb-izal-20240703-111957"
 
-  service_accounts = {
-    "cluster-service-account" = {
-      display_name = "Cluster SA"
-      roles        = [
-        "roles/viewer",
-        "roles/storage.admin"
-      ]
-    },
-    "jenkins-gke" = {
-      display_name = "Jenkins builder SA"
-      roles        = [
-        "roles/cloudkms.admin",
-        "roles/cloudkms.verifier",
-        "roles/container.admin",
-        "roles/resourcemanager.projectIamAdmin",
-        "roles/owner",
-        "roles/iam.serviceAccountAdmin",
-        "roles/iam.serviceAccountTokenCreator",
-        "roles/storage.admin"
-      ]
-    }
-#     "service-376765086748@gs-project-accounts.iam.gserviceaccount.com" = {
-#       display_name = "Cloud Storage SA"
+
+#   service_accounts = {
+#     "cluster-service-account" = {
+#       display_name = "Cluster SA"
 #       roles        = [
-#         "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+#         "roles/viewer",
+#         "roles/storage.admin"
+#       ]
+#     },
+#     "jenkins-gke" = {
+#       display_name = "Jenkins builder SA"
+#       roles        = [
+#         "roles/cloudkms.admin",
+#         "roles/cloudkms.verifier",
+#         "roles/container.admin",
+#         "roles/resourcemanager.projectIamAdmin",
+#         "roles/owner",
+#         "roles/iam.serviceAccountAdmin",
+#         "roles/iam.serviceAccountTokenCreator",
+#         "roles/storage.admin"
 #       ]
 #     }
   }
-}
-
-# output "service_account_emails" {
-#   value = module.service_accounts.service_account_emails
-# }
