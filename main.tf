de371065375d66d6519bd1c1ab7e180359060dbb
izaleski <iwo.zaleski@payback.net>
2024-07-12 11:42:21 +0200

    Code cleanup

--- main.tf
+++ main.tf
@@ -24,28 +24,6 @@ module "IAM" {
     "roles/iam.serviceAccountTokenCreator"  = "serviceAccount:jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
     "roles/storage.admin"                   = "serviceAccount:jenkins-gke@sb-izal-20240703-111957.iam.gserviceaccount.com"
   }
-
-  #   service_accounts = {
-  #     "cluster-service-account" = {
-  #       display_name = "Cluster SA"
-  #       roles        = [
-  #         "roles/viewer",
-  #         "roles/storage.admin"
-  #       ]
-  #     },
-  #     "jenkins-gke" = {
-  #       display_name = "Jenkins builder SA"
-  #       roles        = [
-  #         "roles/cloudkms.admin",
-  #         "roles/cloudkms.verifier",
-  #         "roles/container.admin",
-  #         "roles/resourcemanager.projectIamAdmin",
-  #         "roles/owner",
-  #         "roles/iam.serviceAccountAdmin",
-  #         "roles/iam.serviceAccountTokenCreator",
-  #         "roles/storage.admin"
-  #       ]
-  #     }
 }
 
 output "service_account_role_output" {
